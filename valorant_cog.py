e941655c815ce12e23ee5c10230ebd6d65fe4881
Sean Chen <seanchen@cs.utexas.edu>
2021-06-07 06:03:12 -0500

    formatting

--- valorant_cog.py
+++ valorant_cog.py
@@ -416,13 +416,13 @@ class Valorant(commands.Cog):
                 print(len(history))
                 for match in history:
                     output.append(f"`{match['time'].strftime(time_format)}: ")
-                    output.append(', '.join([(ctx.guild.get_member(int(uid)).name if (int(uid) != int(userid)) else f"__{ctx.guild.get_member(int(uid)).name}__") for uid in match['attackers']]))
+                    output.append(', '.join([(ctx.guild.get_member(int(uid)).name for uid in match['attackers']]))
                     output.append(f" { match['attacker_score']} - {match['defender_score']} ")
-                    output.append(','.join([(ctx.guild.get_member(int(uid)).name if (int(uid) != int(userid)) else f"__{ctx.guild.get_member(int(uid)).name}__") for uid in match['defenders']]))
+                    output.append(','.join([(ctx.guild.get_member(int(uid)).name for uid in match['defenders']]))
                     if userid in match['attackers']:
-                        output.append(f"` {round(match['old_ratings'][userid].mu, 2)} -> {round(match['attackers'][userid].mu, 2)}\n")
+                        output.append(f" ({round(match['old_ratings'][userid].mu, 2)} -> {round(match['attackers'][userid].mu, 2)})`\n")
                     else:
-                        output.append(f"` {round(match['old_ratings'][userid].mu, 2)} -> {round(match['defenders'][userid].mu, 2)}\n")
+                        output.append(f" ({round(match['old_ratings'][userid].mu, 2)} -> {round(match['defenders'][userid].mu, 2)})`\n")
 
             else:
                 history = db['history'][-10:]
