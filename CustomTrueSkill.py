7a921f504714c8bf5efe01b7f8a46448c05488a2
Sean Chen <seanchen@cs.utexas.edu>
2021-06-06 21:07:07 -0500

    fixed rate with dictionary interface

--- CustomTrueSkill.py
+++ CustomTrueSkill.py
@@ -1,14 +1,14 @@
 from typing import Iterable
 import trueskill as ts
 
-def rate_with_round_score(winners:Iterable, losers:Iterable, winner_score:int, loser_score:int, factor=1):
+def rate_with_round_score(winners, losers, winner_score:int, loser_score:int, factor=1):
     score_diff = winner_score - loser_score
     weight_change = 1 + (score_diff/(winner_score)-0.5)*factor
     print(weight_change)
     w_after, l_after = ts.rate([winners, losers], ranks=[0, 1])
-    w_diffs = [w_after[i].mu - winners[i].mu for i in range(len(winners))]
-    l_diffs = [l_after[i].mu - losers[i].mu for i in range(len(losers))]
-    w_after = [ts.Rating(mu=winners[i].mu + w_diffs[i]*weight_change, sigma=w_after[i].sigma) for i in range(len(winners))]
-    l_after = [ts.Rating(mu=losers[i].mu + l_diffs[i]*weight_change, sigma=l_after[i].sigma) for i in range(len(losers))]
+    w_diffs = {id : w_after[id].mu - winners[id].mu for id in winners}
+    l_diffs = {id : l_after[id].mu - losers[id].mu for id in losers}
+    w_after = {id : ts.Rating(mu=winners[id].mu + w_diffs[id]*weight_change, sigma=w_after[id].sigma) for id in winners}
+    l_after = {id : ts.Rating(mu=losers[id].mu + l_diffs[id]*weight_change, sigma=l_after[id].sigma) for id in losers}
     return w_after, l_after
     
