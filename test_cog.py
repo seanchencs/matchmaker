9a71bcef1b4e6626b025612a4144b10f13c34758
Sean Chen <seanchen@cs.utexas.edu>
2021-06-09 16:30:40 -0500

    added delete to test_cog

--- test_cog.py
+++ test_cog.py
@@ -1,7 +1,13 @@
-from main import record_result
+import os
 import random
+
 from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
+from discord_slash import SlashContext, cog_ext
+from discord_slash.model import SlashCommandPermissionType
+from discord_slash.utils.manage_commands import (create_option,
+                                                 create_permission)
+
+from main import record_result
 
 GUILDS = [825900837083676732]
 
@@ -10,15 +16,29 @@ class Test(commands.Cog):
         """Generate test matches."""
         self.bot = bot
     
-    @cog_ext.cog_slash(name='test', guild_ids=GUILDS, description='generate 10 random 1v1 results')
-    async def _test(self, ctx: SlashContext):
+    @cog_ext.cog_slash(name='test', guild_ids=GUILDS, description='generate random 1v1 results', options=[
+        create_option(
+            name='count',
+            description='# of results to generate',
+            option_type=4,
+            required=False,
+        )
+    ])
+    async def _test(self, ctx: SlashContext, count=5):
         members = [member.id for member in ctx.guild.members]
-        for _ in range(10):
+        for _ in range(count):
             players = random.sample(members, 2)
             attacker, defender = players[0], players[1]
             attacker_score = random.choice((13, random.randint(0, 11)))
             defender_score = random.randint(0, 11) if attacker_score == 13 else 13
             record_result([attacker], [defender], attacker_score, defender_score, ctx.guild.id)
+        await ctx.send("✅")
+
+    @cog_ext.cog_slash(name='delete', guild_ids=GUILDS, description='delete the database for this server', permissions={
+        825900837083676735: create_permission(263745246821744640, SlashCommandPermissionType.USER, True)
+    })
+    async def _delete(self, ctx:SlashContext):
+        os.remove(f'{ctx.guild.id}.db')
         await ctx.send("✅")
 
 def setup(bot):
