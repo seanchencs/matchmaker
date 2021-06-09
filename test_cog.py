6cee21fd3453f6b890d719e6988c77efc1b8d39e
SeanChen10 <>
2021-06-09 13:35:00 +0000

    small fixes

--- test_cog.py
+++ test_cog.py
@@ -2,8 +2,6 @@ from main import record_result
 from random import random
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
-from discord_slash.utils.manage_commands import create_option, create_choice
-from main import record_result
 
 GUILDS = [825900837083676732]
 
@@ -19,3 +17,6 @@ class Test(commands.Cog):
             players = random.sample(members, 2)
             attacker, defender = players[0], players[1]
             record_result([attacker], [defender], random.randint(0, 13), random.randint(0, 13), ctx.guild.id)
+
+def setup(bot):
+    bot.add_cog(Test(bot))
