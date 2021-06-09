505240c52625afab634754e05831468e89bc3900
SeanChen10 <>
2021-06-09 13:44:12 +0000

    added output to test command

--- test_cog.py
+++ test_cog.py
@@ -10,13 +10,14 @@ class Test(commands.Cog):
         """Generate test matches."""
         self.bot = bot
     
-    @cog_ext.cog_slash(name='test', guild_ids=GUILDS, description='test')
+    @cog_ext.cog_slash(name='test', guild_ids=GUILDS, description='generate 10 random 1v1 results')
     async def _test(self, ctx: SlashContext):
         members = [member.id for member in ctx.guild.members]
         for _ in range(10):
             players = random.sample(members, 2)
             attacker, defender = players[0], players[1]
             record_result([attacker], [defender], random.randint(0, 13), random.randint(0, 13), ctx.guild.id)
+        await ctx.send("✅")
 
 def setup(bot):
     bot.add_cog(Test(bot))
