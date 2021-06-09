5e7d802f061104abc22a567237c91e7d24495eae
Sean Chen <seanchen@cs.utexas.edu>
2021-06-09 09:16:17 -0500

    debug print

--- valorant_cog.py
+++ valorant_cog.py
@@ -449,7 +449,6 @@ class Valorant(commands.Cog):
                     all_past_ratings = [[val for val in past_ratings for _ in (0, 1)] for past_ratings in all_past_ratings]
                 if all_past_ratings and len(all_past_ratings[0]) > 60:
                     all_past_ratings = [past_ratings[::len(past_ratings)//30] for past_ratings in all_past_ratings]
-                    print(all_past_ratings)
                 output.append('`' + plot(all_past_ratings) + '`\n')
                 for match in history:
                     # match info
