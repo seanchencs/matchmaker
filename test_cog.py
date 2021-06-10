36f869143307ddd2bb6fc61c30a820f4071e0954
Sean Chen <seanchen@cs.utexas.edu>
2021-06-09 20:09:14 -0500

    2v2 testing mode added

--- test_cog.py
+++ test_cog.py
@@ -16,7 +16,23 @@ class Test(commands.Cog):
         """Generate test matches."""
         self.bot = bot
     
-    @cog_ext.cog_slash(name='test', guild_ids=GUILDS, description='generate random 1v1 results', options=[
+    @cog_ext.cog_slash(name='test', guild_ids=GUILDS, description='generate random results', options=[
+        create_option(
+            name='type',
+            description='type of games to make',
+            option_type=3,
+            required=False,
+            choices=[
+                create_choice(
+                    name='1v1',
+                    value='1v1'
+                ),
+                create_choice(
+                    name='2v2',
+                    value='2v2'
+                )
+            ]
+        ),
         create_option(
             name='count',
             description='# of results to generate',
@@ -26,14 +42,18 @@ class Test(commands.Cog):
     ], permissions={
         825900837083676735: create_permission(263745246821744640, SlashCommandPermissionType.USER, True)
     })
-    async def _test(self, ctx: SlashContext, count=5):
+    async def _test(self, ctx: SlashContext, game_type='1v1', count=5):
         members = [member.id for member in ctx.guild.members]
         for _ in range(count):
-            players = random.sample(members, 2)
-            attacker, defender = players[0], players[1]
+            if game_type == '1v1':
+                players = random.sample(members, 2)
+                attacker, defender = [players[0]], [players[1]]
+            elif game_type == '2v2':
+                players = random.sample(members, 4)
+                attacker, defender = [players[0], players[1]], [players[2], players[3]]
             attacker_score = random.choice((13, random.randint(0, 11)))
             defender_score = random.randint(0, 11) if attacker_score == 13 else 13
-            record_result([attacker], [defender], attacker_score, defender_score, ctx.guild.id)
+            record_result(attacker, defender, attacker_score, defender_score, ctx.guild.id)
         await ctx.send("✅")
 
     @cog_ext.cog_slash(name='delete', guild_ids=GUILDS, description='delete the database for this server', permissions={
