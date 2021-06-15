e043258b8253d360f5485f73b6470bae7d0a4b03
Sean Chen <seanchen@cs.utexas.edu>
2021-06-15 15:04:52 -0500

    more fix

--- valorant_cog.py
+++ valorant_cog.py
@@ -435,9 +435,9 @@ class Valorant(commands.Cog):
                 w, l = get_win_loss(item[0], ctx.guild.id)
                 rank += 1
                 if last[0] and ((metric == 'exposure' and isclose(ts.expose(item[1]), ts.expose(last[0]))) or (metric == 'mean' and isclose(item[1], last[0]))):
-                    output.append([last[1], member.name, f'{item[1].mu, 4: .4f} ± {item[1].sigma: .4f}', round(ts.expose(item[1]), 4), f'{w}W {l}L'])
+                    output.append([last[1], member.name, f'{item[1].mu: .4f} ± {item[1].sigma: .4f}', round(ts.expose(item[1]), 4), f'{w}W {l}L'])
                 else:
-                    output.append([rank, member.name, f'{item[1].mu, 4: .4f} ± {item[1].sigma: .4f}', round(ts.expose(item[1]), 4), f'{w}W {l}L'])
+                    output.append([rank, member.name, f'{item[1].mu: .4f} ± {item[1].sigma: .4f}', round(ts.expose(item[1]), 4), f'{w}W {l}L'])
                 last = item[1], rank
         await ctx.send(f"`Leaderboard (by {metric}):\n{tabulate(output, headers=headers, tablefmt='psql', floatfmt='.4f')}`")
 
